e8aab79abbce23bf3cdd26b3b6093b8cfae6ed54
EmmaRosby <U2457534@rts-365.dk>
2025-09-01 10:15:26 +0200

    Start mandag morgen

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: {
+    template: "%s | Landrup Dans",
+    default: "Landrup Dans",
+  },
+  description: "Landrup Dans er en lille danseskole for både børn og voksne",
+}
+export default function Home() {
+  return (
+    <html lang="da">
+      <body>
+        
+      </body>
+    </html>
+  );
+}
